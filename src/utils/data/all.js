8c53d29163aa6b796431a0eae0243d3330700ae3
eusoumaxi <d99maxi@gmail.com>
2020-07-16 13:27:06 -0500

    updated: all.js -> Home

--- src/utils/data/all.js
+++ src/utils/data/all.js
@@ -1,75 +1,86 @@
 const { config } = require('../../config');
 
 module.exports = {
-  Documentation: 'https://bit.ly/api-corona',
-  SourceData: {
-    Description:
+  documentation: 'https://bit.ly/api-corona',
+  sourceData: {
+    description:
       'Access data on COVID19 through an easy API for free. Build dashboards, mobile apps or integrate in to other applications. Data is sourced from Johns Hopkins CSSE',
     getData:
       'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data',
   },
-  notice: 'From August 1, the url api-corona.azurewebsites.net will cease to exist. the new url is: https://corona.azure-api.net/',
-  buyMeACoffe: 'https://bit.ly/3gORfVw',
-  GraphQL: {
-    Name: 'GraphQL',
-    Path: 'https://api-corona.azurewebsites.net/graphql',
+  notice: {
+    message:
+      'From August 1, the url api-corona.azurewebsites.net will cease to exist. the new url is: https://corona.azure-api.net/',
+    urlNew: 'https://corona.azure-api.net/',
+  },
+  donations: {
+    message:
+      'Currently we have no advertising, partner and this project is hosted in azure, the way to pay for this service is by donations.',
+    paypal: 'https://paypal.me/covid19server',
+    buyMeACoffe: 'https://bit.ly/3gORfVw',
+  },
+  graphQL: {
+    name: 'GraphQL',
+    client: 'https://api-corona.azurewebsites.net/graphql',
+    POST: `${config.url}/graphql`,
   },
   allRoute: {
-    Name: 'Get All Data',
-    Description: 'Returns all data in the system.',
-    Path: `${config.url}/all`,
+    name: 'Get All Data',
+    description: 'Returns all data in the system.',
+    path: `${config.url}/all`,
   },
   countriesRoute: {
-    Name: 'Get List Of Countries.',
-    Description:
+    name: 'Get List Of Countries.',
+    description:
       'Returns all countries and associated provinces. The country_slug variable is used for country specific data',
-    Path: `${config.url}/country`,
+    path: `${config.url}/country`,
   },
   countryRoute: {
-    Name: 'Get data per country. ',
-    Description:
+    name: 'Get data per country. ',
+    description:
       'Returns data from the country, state and city (in some cases).',
     Path: '/country/:country',
     Example: `${config.url}/country/italy`,
   },
   countryRouteState: {
-    Name: 'Get data per country and associated state. ',
-    Description: 'Returns state case data',
+    name: 'Get data per country and associated state. ',
+    description: 'Returns state case data',
     Path: '/country/:country/:state',
     Example: `${config.url}/country/spain/andalusia`,
   },
   countryRouteStateCity: {
-    Name: 'Get data per city.',
-    Description: 'Returns case data to the city. Only valid for united states',
+    name: 'Get data per city.',
+    description: 'Returns case data to the city. Only valid for united states',
     Path: '/country/:country/:state/:city',
     Example: `${config.url}/country/us/california/Los%20Angeles`,
   },
   timeline: {
-    Name: 'Get the timeline of all countries.',
-    Description:
+    name: 'Get the timeline of all countries.',
+    description:
       'Returns all data recorded in the country, state, and city timeline system.',
     Path: `${config.url}/timeline`,
   },
   timelinePerCountry: {
-    Name: 'Get the timeline per country.',
-    Description: 'Returns the timeline by case type for a country.',
+    name: 'Get the timeline per country.',
+    description: 'Returns the timeline by case type for a country.',
     Path: '/timeline/:country',
     Example: `${config.url}/timeline/united-states`,
   },
   filters: {
     name:
       'Returns data filtered by dates, and if you add another endDate it returns the date range',
-    Description: `${config.url}/filters`,
+    description: `${config.url}/filters`,
     ExampleOne: `${config.url}/filters?date=01-22-2020&country=canada`,
     ExampleSecons: `${config.url}/filters?date=06-22-2020&endDate=06-25-2020&country=co`,
   },
   summaryRoute: {
-    Name: 'Summary of new and total cases per country',
-    Description: 'A summary of new and total cases per country',
+    name: 'Summary of new and total cases per country',
+    description: 'A summary of new and total cases per country',
     Path: `${config.url}/summary`,
   },
   autor: {
-    User: '@eusoumaxi',
-    Link: 'https://github.com/eusoumaxi',
+    name: 'David Lara',
+    user: '@eusoumaxi',
+    gitHub: 'https://github.com/eusoumaxi',
   },
 };
