e01cdbc2b7219f96310507e0e97e332d6d9f9a6d
d99maxi <d99maxi@pop-os.localdomain>
2020-07-20 23:58:53 -0500

    add: robot.txt

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const path = require('path');
 const cors = require('cors');
 const favicon = require('serve-favicon');
+const robots = require('express-robots-txt');
 const api = require('./routes/api');
 const {
   logErrors,
@@ -22,6 +23,7 @@ app.use(favicon(path.join(__dirname, '../favicon.ico')));
 
 // body parser
 app.use(express.json());
+app.use(robots({ UserAgent: '*', Disallow: '/' }));
 
 // Middleware
 app.use(helmet());
